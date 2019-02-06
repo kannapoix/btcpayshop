df35f5199b3bd4a85339935fe25350cfd4d87acd
kanna <kanna@vivaldi.net>
2019-02-06 22:49:47 +0900

    prepare for adding images

--- item/models.py
+++ item/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils import timezone
 from shop.models import Shop
 
+def shop_directory_path(instance, filename):
+    return 'shop_{0}/{1}'.format(instance.shop.id, filename)
+
 class Item(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    btcpay_item_name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    image_path = models.FileField(upload_to=shop_directory_path, null=True)
     is_active = models.BooleanField()
     created_date = models.DateTimeField(
             default=timezone.now)
